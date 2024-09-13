be48e70da72edd6bdcd2943d1faee47f093eeedc
aarya-kumar <aarya.kumar1712@gmail.com>
2024-09-13 06:51:45 +0530

    added middleware and cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,21 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try{
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken() 
+        const refreshToken = user.generateRefreshToken() 
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+
+    } catch(error) {
+        throw new ApiError(500, "Something went wrong while generating access and refresh token")
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
 
@@ -35,7 +50,7 @@ const registerUser = asyncHandler(async (req, res) => {
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
-    } 
+    }
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
@@ -69,9 +84,81 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // return res 
     return res.status(201).json(
-        new ApiResponse(200, createdUser, "User registered successfully") 
+        new ApiResponse(200, createdUser, "User registered successfully")
     )
 })
 
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, username, password } = req.body
 
-export { registerUser }
+    if (!username || !email) {
+        throw new ApiError(400, "username or email is required") 
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "user does not exist") 
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password) 
+    if(!isPasswordValid){
+        throw new ApiError(401, "invalid user credentials") 
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUSer = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true, 
+        secure: true 
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUSer, accessToken, refreshToken
+            }, 
+            "User logged in successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true 
+        }
+    )
+
+    const options = {
+        httpOnly: true, 
+        secure: true 
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out"))
+})
+
+export { 
+    registerUser, 
+    loginUser,
+    logoutUser
+}
