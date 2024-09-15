b5b75f0ae5651cb07a762ab30d34dae8386b5ecd
aarya-kumar <aarya.kumar1712@gmail.com>
2024-09-15 19:26:48 +0530

    fix user controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -225,7 +225,8 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
-        .json(200, req.user, "current user fetched successfully")
+        .json(new ApiResponse(
+            200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -235,12 +236,12 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
                 fullName,
-                email
+                email: email 
             }
         },
         { new: true }
@@ -279,7 +280,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
         .status(200)
         .json(
             new ApiResponse(200, user, "Avatar image updated successfully")
-    )
+        )
 })
 
 const updateUserCoverImage = asyncHandler(async (req, res) => {
@@ -309,7 +310,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         .status(200)
         .json(
             new ApiResponse(200, user, "Cover image updated successfully")
-    )
+        )
 })
 
 export {
